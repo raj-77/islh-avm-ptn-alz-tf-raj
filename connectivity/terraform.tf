e9cbe36ce5d02f4defafcad6ea7cd157b5780aa0
raj-77 <36180789+raj-77@users.noreply.github.com>
2025-08-12 23:14:37 -0700

    Create terraform.tf

---
+++ connectivity/terraform.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.9, < 2.0"
+  required_providers {
+    alz = {
+      source  = "azure/alz"
+      version = "~> 0.17"
+    }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~> 2.0, >= 2.0.1"
+    }
+
+  }
+
+}
+
+provider "alz" {
+  library_references = [
+    {
+      path = "platform/alz"
+      ref  = "2025.01.0"
+    },
+    {
+      custom_url = "${path.root}/lib"
+    }
+  ]
+}
